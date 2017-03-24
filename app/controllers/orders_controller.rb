54ed6992cc5617c53d0c31833a69ffccb5ae70a5
activeliang <activeliang@gmail.com>
2017-03-24 19:13:19 +0800

    for demo

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,21 +16,24 @@ class OrdersController < ApplicationController
         product_list.save
       end
 
+      current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
+
       redirect_to order_path(@order.token), notice:"成功生成订单"
     else
       redirect_to :back
     end
   end
 
-  def show
-    @order = Order.find_by_token(params[:id])
-    @product_lists = @order.product_lists
-  end
+   def show
+     @order = Order.find_by_token(params[:id])
+     @product_lists = @order.product_lists
+   end
 
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
-    @order.pay!
+    @order.make_payment!
 
     redirect_to order_path(@order.token), notice: "使用微信成功完成付款！"
   end
@@ -38,9 +41,15 @@ class OrdersController < ApplicationController
   def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!"alipay"
-    @order.pay!
+    @order.make_payment!
 
     redirect_to order_path(@order.token),notice: "使用支付宝成功完成付款!"
+  end
+
+  def apply_to_cancel
+    @order = Order.find(params[:id])
+    OrderMailer.apply_cancel(@order).deliver!
+    redirect_to :back, notice: "已提交申请！"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
   end
 
   resources :products do
@@ -26,7 +34,8 @@ Rails.application.routes.draw do
     member do
     post :pay_with_wechat
     post :pay_with_alipay
-  end 
+    post :apply_to_cancel
+  end
   end
 
   namespace :account do
