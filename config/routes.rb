fb3bcb016f160a3a63b2abc344b01847ad713060
activeliang <activeliang@gmail.com>
2017-05-14 20:45:34 +0800

    for test

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,16 @@ Rails.application.routes.draw do
     resources :orders
   end
 
+  resources :payments, only: [:index] do
+    collection do
+      get :generate_pay
+      get :pay_return
+      get :pay_notify
+      get :success
+      get :failed
+    end
+end
+
 
 
 end
