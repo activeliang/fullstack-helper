74e76fce12eeae7925b00e51138061477391ba55
chenxiyuan801 <chenxiyuan801@gmail.com>
2017-05-24 17:58:21 +0800

    Send_sms module build up

---
+++ lib/send_sms.rb
@@ -0,0 +1,53 @@
+module SendSMS
+  class << self
+
+    MESSAGE_SUFFIX = "【全栈助手】"
+    SMS_API_KEY = ENV['ff109241119f355168e47ff9e5a9cb70']
+    SMS_SEND_URL = "https://sms-api.luosimao.com/v1/send.json"
+    SMS_STATUS_URL = "https://sms-api.luosimao.com/v1/status.json"
+
+    #
+    # SendSMS.send "13718781273", "hello"
+    # SendSMS.status
+    #
+
+    def send mobile, message
+      options = {
+        mobile: mobile,
+        message: "#{message}#{MESSAGE_SUFFIX}"
+      }
+
+      begin
+        response = RestClient.post SMS_SEND_URL, options, headers
+        response = JSON.parse(response)
+        Rails.logger.info "SMS: mobile #{mobile}, options: #{options}, response: #{response}"
+      rescue => e
+        response = e.message
+      end
+
+      unless response.try(:fetch, "error") == 0
+        Rails.logger.info "SMS failed: mobile #{mobile}, options: #{options}, response: #{response}"
+      end
+
+      response
+    end
+
+    def status
+      begin
+        response = RestClient.get SMS_STATUS_URL, headers
+        response = JSON.parse(response)
+        Rails.logger.info "SMS: status #{response}"
+      rescue => e
+        response = e.message
+      end
+
+      response
+    end
+
+    private
+    def headers
+      { Authorization: "Basic " + Base64.encode64("api:key-#{SMS_API_KEY}").chomp }
+    end
+
+  end
+end
