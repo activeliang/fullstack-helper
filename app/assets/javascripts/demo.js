1002f4a6df7a5705eae4ae42a0f9b6362b30a219
chenxiyuan801 <chenxiyuan801@gmail.com>
2017-06-02 16:41:30 +0800

    stepback to normal signup page

--- app/assets/javascripts/demo.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
