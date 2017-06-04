ea7477b26679c69fae77427d849f6818869d17ab
iaqgypmb <1206210445@qq.com>
2017-06-04 15:52:26 +0800

    change poroduct_list_image method

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -20,7 +20,7 @@ class Payment < ApplicationRecord
       self.status = Payment::PaymentStatus::Success
       self.raw_response = params.to_json
       self.payment_at = Time.now
-      self.save!
+      self.save
 
       # 更新订单状态
       self.orders.each do |order|
@@ -35,14 +35,15 @@ class Payment < ApplicationRecord
           order.make_payment!
           order.is_paid = true
           order.payment_method = "alipay"
-          order.save!
+          order.save
 
         if !order.of_lesson?
           order.product_lists.each do |product_list|
-            product_list.lists_image = subproduct.subproduct_image.thumb
+
             product = Product.find(product_list.product_id)
+
             product.sales_count += 1
-            product.save!
+            product.save
           end
         end
 
@@ -51,16 +52,34 @@ class Payment < ApplicationRecord
 
           lesson = Lesson.find(order.lesson_id)
           lesson.sales_count += 1
-          lesson.save!
+          lesson.save
 
           buyer = Buyer.new
           buyer.price = lesson.price
           buyer.user_id = order.user_id
           buyer.lesson_id = order.lesson_id
-          buyer.save!
+          buyer.save
         end
 
       end
+    end
+  end
+
+  def do_success_image_migration
+    self.orders.each do |order|
+
+        order.product_lists.each do |product_list|
+          if !order.of_lesson?
+            product = Product.find(product_list.product_id)
+            product_list.lists_image = product.main_product_photo.product_image.thumb
+          end
+          if order.of_lesson?
+            lesson = Lesson.find(order.lesson_id)
+            product_list.lists_image = lesson.main_image.thumb
+          end
+          product_list.save
+        end
+      
     end
   end
 
